2db05def2e4b29805cbae5aa8c2e5ebcd703033f
TA Nguyen <tuananh.nguyen@nih.gov>
2026-03-04 09:47:03 -0500

    USWDS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import '@uswds/uswds/css/uswds.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
